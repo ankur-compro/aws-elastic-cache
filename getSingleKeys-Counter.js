f6b95fc1067fca405f69da354ca234de73a2b3ed
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-01-22 15:12:16 +0530

    Create getSingleKeys-Counter.js

---
+++ getSingleKeys-Counter.js
@@ -0,0 +1,46 @@
+var redis = require("redis");
+var timeout = 1; // = 1 sec, (in miliseconds)
+var counter = 0;
+
+var options = {
+    'host': 'test-redis.dwnzoe.ng.0001.usw2.cache.amazonaws.com',
+    'port': 6379
+};
+
+var client = redis.createClient(options.port, options.host);
+
+client.on('connect', function () {
+    console.log('Redis client connected');
+});
+
+client.on('ready', function () {
+    console.log('Redis client ready');
+});
+
+client.on('error', function (err) {
+    console.log('Something went wrong ' + err);
+});
+
+var redisKey = 's:PrimaryNodeTesting:22Jan:1';
+
+
+var previousValue;
+
+console.log('GETting key - ' + redisKey);
+var intervalId = setInterval(function() {
+    counter++;
+    var timestamp = Date.now();
+    client.get(redisKey, function (error, result) {
+        if (error) {
+          console.log("ERROR at getting-----");
+          console.log(error);
+        }
+        else {
+            
+            if(previousValue !== result) {
+                console.log('Received-' + Date.now() + ' === ' + result + ' : ' + timestamp, timestamp - parseInt(result));
+                previousValue = result;
+            }
+        }
+    });
+}, timeout);
