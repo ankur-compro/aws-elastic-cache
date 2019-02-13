fa903facec705aadcf49bddb3a5d9e15562bc5e9
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-02-13 13:53:26 +0530

    Create setSingleKey.js

---
+++ cluster-mode-enabled/setSingleKey.js
@@ -0,0 +1,34 @@
+var redis = require("redis");
+
+var options = {
+  "host": "test-cluster-enabled.dwnzoe.clustercfg.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+console.log('Creating Redis Client');
+var client = redis.createClient(options.port, options.host);
+
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.on('connect', function() {
+  console.log('Redis connect event');
+});
+
+client.on('ready', function() {
+  console.log('Redis ready event');
+});
+
+var redisKey = 's:cluster-enabled:config-url:1';
+console.log(redisKey);
+ 
+var timestamp = Date.now();
+client.set(redisKey, timestamp, function(err, response) {
+  if(err) {
+    console.log('error while setting redis key');
+    console.log(err);
+  } else {
+    console.log('SET Redis Value : ' + timestamp);
+  }
+});
