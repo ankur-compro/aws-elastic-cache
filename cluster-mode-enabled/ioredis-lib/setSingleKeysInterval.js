375d3ad2e49f1c440e6e6504f037eb576624f304
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-02-13 19:12:04 +0530

    Create setSingleKeysInterval.js

---
+++ cluster-mode-enabled/ioredis-lib/setSingleKeysInterval.js
@@ -0,0 +1,40 @@
+//var redis = require("redis");
+var Redis = require('ioredis');
+
+var options = {
+  "host": "test-clstr-enabled-3.dwnzoe.clustercfg.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+var redis = new Redis(options);
+
+console.log('Creating Redis Client');
+
+redis.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+redis.on('connect', function() {
+  console.log('Redis connect event');
+});
+
+redis.on('ready', function() {
+  console.log('Redis ready event');
+});
+
+var redisKey = 's:cluster-enabled-2:config:1';
+console.log(redisKey);
+ 
+var timestamp = Date.now();
+
+setInterval(
+  function() {
+    redis.set(redisKey + ':' + Date.now(), timestamp, function(err, response) {
+      if(err) {
+        console.log('error while setting redis key');
+        console.log(err);
+      } else {
+        console.log('SET Redis Value : ' + timestamp);
+      }
+    });
+}, 1000);
