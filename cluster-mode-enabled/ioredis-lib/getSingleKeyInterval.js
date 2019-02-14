5947870cc6f0f1cc1a1014244c17e0aed8ae57eb
Anupam Sharma <anupam.sharma@comprotechnologies.com>
2019-02-14 18:10:29 +0530

    Create getSingleKeyInterval.js

---
+++ cluster-mode-enabled/ioredis-lib/getSingleKeyInterval.js
@@ -0,0 +1,38 @@
+var Redis = require('ioredis');
+
+var options = {
+  "host": "test-cluster-enabled.dwnzoe.clustercfg.usw2.cache.amazonaws.com",
+  "port": 6379
+};
+
+//var redis = new Redis(options);
+
+var redis = new Redis.Cluster([options]);
+
+console.log('Creating Redis Client');
+
+redis.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+redis.on('connect', function() {
+  console.log('Redis connect event');
+});
+
+redis.on('ready', function() {
+  console.log('Redis ready event');
+});
+
+var redisKey = 's:cluster-enabled:config:1';
+console.log(redisKey);
+ 
+setInterval(function() {
+  redis.get(redisKey, function(err, response) {
+    if(err) {
+      console.log('error while getting redis key');
+      console.log(err);
+    } else {
+      console.log('GET Redis Key : ' + response);
+    }
+  });
+}, 500);
