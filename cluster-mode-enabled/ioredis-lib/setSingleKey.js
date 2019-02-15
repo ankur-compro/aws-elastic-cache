bf05a447acd4e746ec2b12fd11a14ea3b9c67744
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-02-15 16:45:55 +0530

    Update setSingleKey.js

--- cluster-mode-enabled/ioredis-lib/setSingleKey.js
+++ cluster-mode-enabled/ioredis-lib/setSingleKey.js
@@ -2,7 +2,7 @@
 var Redis = require('ioredis');
 
 var options = {
-  "host": "test-cluster-enabled.dwnzoe.clustercfg.usw2.cache.amazonaws.com",
+  "host": "test-cluster-enabled-0001-001.dwnzoe.0001.usw2.cache.amazonaws.com",
   "port": 6379
 };
 
@@ -24,18 +24,18 @@ redis.on('ready', function() {
   console.log('Redis ready event');
 });
 
-var redisKey = 's:baljinder';
+var redisKey = 's:anupam:sharma';
 console.log(redisKey);
  
 var setRedisKey = redisKey;
 var timestamp = Date.now();
-redis.set(setRedisKey, timestamp, function(err, response) { 
+redis.set(setRedisKey, 'sharma', function(err, response) { 
   if(err) {
     console.log('error while setting redis key');
     console.log(err);
   } else {
     console.log('SET Redis Key : ' + setRedisKey);
-    console.log('SET Redis Value : ' + timestamp);
+    console.log('SET Redis Value : ' + 'sharma');
   }
 });
 
