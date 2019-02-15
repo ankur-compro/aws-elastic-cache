5ee0b4762c6c4ba3730dbf38ab4ac19c3e8bf704
baljinder-singh <baljinder.singh@comprotechnologies.com>
2019-02-15 13:20:45 +0530

    Update setSingleKey.js

--- cluster-mode-enabled/ioredis-lib/setSingleKey.js
+++ cluster-mode-enabled/ioredis-lib/setSingleKey.js
@@ -24,7 +24,7 @@ redis.on('ready', function() {
   console.log('Redis ready event');
 });
 
-var redisKey = 's:baljinder:meenu';
+var redisKey = 's:baljinder';
 console.log(redisKey);
  
 var setRedisKey = redisKey;
